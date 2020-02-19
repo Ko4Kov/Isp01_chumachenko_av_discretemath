c1fb6267c840508aabdcb40a835dea7b779a0113
Чумаченко Андрей ИСП-1 <60880992+Ko4Kov@users.noreply.github.com>
2020-02-19 17:58:59 +0300

    Update Program.cs

--- self_task/work_10.02.2020/projects/ConsoleApp3/ConsoleApp3/Program.cs
+++ self_task/work_10.02.2020/projects/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,53 +1,52 @@
 ﻿using System;
 
-namespace zada4ki_mojno_men9t_
+namespace sorted
 {
     static class Sort
     {
+        public static void Swap(int[] array, int index, int swap)
+        {
+            int tmp = array[index];
+            array[index] = array[swap];
+            array[swap] = tmp;
+        }
         public static void BubbleSort(int[] array)
         {
             if (array == null || array.Length < 2)
                 return;
+
             bool swapDetected = false;
-            for (int index = 0; index < array.Length; index++)
+            while (true)
             {
                 swapDetected = false;
-                for (int j = index + 1; j < array.Length; j++)
+                for (int index = 0; index < array.Length - 1; index++)
                 {
-                    if (array[index] < array[j])
+                    if (array[index] < array[index + 1])
                     {
-                        int tmp = array[index];
-                        array[index] = array[j];
-                        array[j] = tmp;
+                        Swap(array, index, index + 1);
                         swapDetected = true;
                     }
                 }
                 if (!swapDetected) break;
             }
         }
-        public static void SelectionSort(int[] massiv)
+        public static void SelectionSort(int[] array)
         {
-            if (massiv == null || massiv.Length < 2)
+            if (array == null || array.Length < 2)
                 return;
-            for (int index = 0; index < massiv.Length; index++)
+            for (int index = 0; index < array.Length; index++)
             {
                 int minIndex = index;
-                for (int next = index + 1; next < massiv.Length; next++)
-                {
-                    if (massiv[minIndex] < massiv[next])
+                for (int next = index + 1; next < array.Length; next++)
+                    if (array[minIndex] < array[next])
                         minIndex = next;
-                    if (minIndex != index)
-                    {
-                        int tmp = massiv[index];
-                        massiv[index] = massiv[minIndex];
-                        massiv[minIndex] = tmp;
-                    }
-                }
+                if (minIndex != index)
+                    Swap(array, minIndex, index);
             }
         }
-    }
+    }  
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             Console.WriteLine("Сортировка пузырьком:");
@@ -55,14 +54,14 @@ namespace zada4ki_mojno_men9t_
             Sort.BubbleSort(array);
             foreach (int elem in array)
                 Console.Write(elem + " ");
-           
+
             Console.WriteLine();
 
             Console.WriteLine("Сортировка выбором:");
-            int[] massiv = new int[5] { 2, 5, 3, 8, 11 };
-            Sort.SelectionSort(massiv);
-            foreach (int element in massiv)
+            Sort.SelectionSort(array);
+            foreach (int element in array)
                 Console.Write(element + " ");
         }
     }
 }
+
